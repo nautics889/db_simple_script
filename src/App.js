207a1dd5b98ff98aadfff1316ba70a75f5b89955
nautics889 <cyberukr@gmail.com>
2020-02-15 20:19:27 +0200

    Created basic components (dummies); Added ``react-keyboard-event-handler`` as a dependency;

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
+import KeyboardEventHandler from 'react-keyboard-event-handler';
 import './App.css';
+
+function Output(props) {
+  return (
+    <div>
+      output: {props.output}
+    </div>
+  )
+}
+
+function Input(props) {
+  return (
+    <div>
+      input
+    </div>
+  )
+}
+
+class Console extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: '...',
+      toOutput: '...'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({userInput: e.target.value});
+  }
+
+  render() {
+    return (
+      <div>
+        <Output output={this.state.toOutput}></Output>
+        <KeyboardEventHandler
+          handleKeys={['enter']}
+          handleFocusableElements={true}
+          onKeyEvent={(key, e) => this.setState({toOutput: this.state.userInput})} />
+        <input type="text" defaultValue={this.state.userInput} onChange={this.handleChange} />
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Console />
     </div>
   );
 }
